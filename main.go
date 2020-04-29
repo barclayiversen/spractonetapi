46ffc679a86875fbfc2f55e0bb8d5106e00d2ff8
Barclay SADA <barclay.iversen@sada.com>
2020-04-29 14:45:16 -0700

    Refactor main

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"html/template"
 	"log"
@@ -34,27 +33,4 @@ func main() {
 	r.ServeFiles("/static/*filepath", http.Dir("./static"))
 	fmt.Println("listening on 8000")
 	log.Fatal(http.ListenAndServe(":8000", r))
-}
-
-func wrapHandler(h http.Handler) httprouter.Handle {
-	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		ctx := r.Context()
-
-		ctx = context.WithValue(ctx, "params", ps)
-
-		r = r.WithContext(ctx)
-		h.ServeHTTP(w, r)
-	}
-}
-
-func loginHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		tpl.ExecuteTemplate(w, "login.gohtml", nil)
-	}
-}
-
-func signupHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		tpl.ExecuteTemplate(w, "signup.gohtml", nil)
-	}
 }
