11a34c8e3fd791654ee27f6ab884c96b35912537
Barclay SADA <barclay.iversen@sada.com>
2020-04-29 14:42:52 -0700

    Refactored controller

--- controllers/user.go
+++ controllers/user.go
@@ -4,9 +4,11 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"spractonetapi/driver"
 	"spractonetapi/models"
 	"spractonetapi/repository/userRepository"
 	"spractonetapi/utils"
@@ -14,114 +16,35 @@ import (
 	"strings"
 
 	"github.com/dgrijalva/jwt-go"
+	"github.com/julienschmidt/httprouter"
 	uuid "github.com/satori/go.uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
-func (c Controller) HelloWorld() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		utils.ResponseJSON(w, "Welcome to Spracto net!")
-	}
+type Controller struct {
+	tpl *template.Template
 }
 
-func (c Controller) Signup(db *sql.DB) http.HandlerFunc {
+func NewUserController(tpl *template.Template) *Controller {
+	return &Controller{tpl}
+}
 
-	return func(w http.ResponseWriter, r *http.Request) {
+func (c Controller) HelloWorld(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	c.tpl.ExecuteTemplate(w, "signup.gohtml", nil)
+}
 
-		var user models.User
-		json.NewDecoder(r.Body).Decode(&user)
+func (c Controller) IndexHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	w.WriteHeader(http.StatusOK)
+	c.tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
 
-		if user.Email == "" {
-			utils.RespondWithError(w, http.StatusBadRequest, "Email is missing")
-			return
-		}
-
-		if user.Password == "" {
-			utils.RespondWithError(w, http.StatusBadRequest, "Password is missing")
-			return
-		}
-
-		if user.Username == "" {
-			utils.RespondWithError(w, http.StatusBadRequest, "Username is missing")
-			return
-		}
-
-		EmailIsValid := utils.IsEmail(user)
-
-		if EmailIsValid != true {
-			utils.RespondWithError(w, http.StatusBadRequest, "Please provide a valid Email address IE email@domain.com")
-			return
-		}
-
-		hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
-
-		if err != nil {
-			log.Println(err)
-			utils.RespondWithError(w, http.StatusInternalServerError, "Something went wrong. Please try again later.")
-			return
-		}
-
-		user.Password = string(hash)
-
-		u2 := uuid.Must(uuid.NewV4()).String()
-		if err != nil {
-			fmt.Printf("Something went wrong: %s", err)
-			return
-		}
-
-		user.SignupKey = u2
-		userRepo := userRepository.UserRepository{}
-		user, err = userRepo.Signup(db, user)
-
-		//This is bad
-		if err != nil && err.Error() == "pq: duplicate key value violates unique constraint \"users_email_key\"" {
-			utils.RespondWithError(w, http.StatusBadRequest, "That email is already registered")
-			return
-		}
-
-		if err != nil && err.Error() == "pq: duplicate key value violates unique constraint \"username_is_unique\"" {
-			utils.RespondWithError(w, http.StatusBadRequest, "That username is taken")
-			return
-		}
-
-		if err != nil {
-			utils.RespondWithError(w, http.StatusBadRequest, "Something went wrong")
-			return
-		}
-
-		// Create magic link
-		var tokenString string
-
-		if os.Getenv("ENV") == "dev" {
-			tokenString = "http://localhost:8080/verifyemail?token="
-		}
-
-		if os.Getenv("ENV") == "prod" {
-			tokenString = "https://api.spracto.net/verifyemail?token="
-		}
-
-		fmt.Printf("UUIDv4: %s\n", u2)
-		tokenString += u2
-		tokenString += "&userid="
-		tokenString += strconv.Itoa(user.ID)
-
-		fmt.Println(tokenString)
-
-		err = utils.Send(user, tokenString)
-		if err != nil {
-			utils.RespondWithError(w, http.StatusBadGateway, "We weren't able to send you a verifcation email.")
-			return
-		}
-
+func (c Controller) Login(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	if r.Method == "GET" {
+		c.tpl.ExecuteTemplate(w, "login.gohtml", nil)
 		return
 	}
 
-}
-
-func (c Controller) Login(db *sql.DB) http.HandlerFunc {
-
-	return func(w http.ResponseWriter, r *http.Request) {
-
+	if r.Method == "POST" {
 		var user models.User
 
 		json.NewDecoder(r.Body).Decode(&user)
@@ -129,7 +52,7 @@ func (c Controller) Login(db *sql.DB) http.HandlerFunc {
 		password := user.Password
 
 		userRepo := userRepository.UserRepository{}
-		user, err := userRepo.Login(db, user)
+		user, err := userRepo.Login(driver.DB, user)
 
 		hashedPassword := user.Password
 
@@ -160,8 +83,104 @@ func (c Controller) Login(db *sql.DB) http.HandlerFunc {
 		user.Token = token
 		user.Password = ""
 		utils.ResponseJSON(w, user)
-
 	}
+
+}
+
+func (c Controller) Signup(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+	if r.Method == "GET" {
+		c.tpl.ExecuteTemplate(w, "signup.gohtml", nil)
+		return
+	}
+
+	var user models.User
+	json.NewDecoder(r.Body).Decode(&user)
+
+	if user.Email == "" {
+		utils.RespondWithError(w, http.StatusBadRequest, "Email is missing")
+		return
+	}
+
+	if user.Password == "" {
+		utils.RespondWithError(w, http.StatusBadRequest, "Password is missing")
+		return
+	}
+
+	if user.Username == "" {
+		utils.RespondWithError(w, http.StatusBadRequest, "Username is missing")
+		return
+	}
+
+	EmailIsValid := utils.IsEmail(user)
+
+	if EmailIsValid != true {
+		utils.RespondWithError(w, http.StatusBadRequest, "Please provide a valid Email address IE email@domain.com")
+		return
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+
+	if err != nil {
+		log.Println(err)
+		utils.RespondWithError(w, http.StatusInternalServerError, "Something went wrong. Please try again later.")
+		return
+	}
+
+	user.Password = string(hash)
+
+	u2 := uuid.Must(uuid.NewV4()).String()
+	if err != nil {
+		fmt.Printf("Something went wrong: %s", err)
+		return
+	}
+
+	user.SignupKey = u2
+	userRepo := userRepository.UserRepository{}
+	user, err = userRepo.Signup(driver.DB, user)
+
+	//This is bad
+	if err != nil && err.Error() == "pq: duplicate key value violates unique constraint \"users_email_key\"" {
+		utils.RespondWithError(w, http.StatusBadRequest, "That email is already registered")
+		return
+	}
+
+	if err != nil && err.Error() == "pq: duplicate key value violates unique constraint \"username_is_unique\"" {
+		utils.RespondWithError(w, http.StatusBadRequest, "That username is taken")
+		return
+	}
+
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Something went wrong")
+		return
+	}
+
+	// Create magic link
+	var tokenString string
+
+	if os.Getenv("ENV") == "dev" {
+		tokenString = "http://localhost:8080/verifyemail?token="
+	}
+
+	if os.Getenv("ENV") == "prod" {
+		tokenString = "https://api.spracto.net/verifyemail?token="
+	}
+
+	fmt.Printf("UUIDv4: %s\n", u2)
+	tokenString += u2
+	tokenString += "&userid="
+	tokenString += strconv.Itoa(user.ID)
+
+	fmt.Println(tokenString)
+
+	err = utils.Send(user, tokenString)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadGateway, "We weren't able to send you a verifcation email.")
+		return
+	}
+
+	return
+
 }
 
 func (c Controller) TokenVerifyMiddleware(next http.HandlerFunc) http.HandlerFunc {
