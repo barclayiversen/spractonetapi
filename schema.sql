52aa3a2e88e6fbf7a4a2440b35b2454267c6d927
Barclay SADA <barclay.iversen@sada.com>
2020-04-30 14:10:03 -0700

    Quality of life
    Auto reload with "air" and schema defined.

---
+++ schema.sql
@@ -0,0 +1,22 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255),
+    password VARCHAR(255),
+    username VARCHAR(255),
+    created_at BIGINT,
+    activated BOOLEAN,
+    activation_key VARCHAR(255)
+);
+
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    post VARCHAR(2000),
+    user_id INT REFERENCES users (id),
+    created_at BIGINT
+);
+
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    comment VARCHAR(300),
+    post_id INT REFERENCES posts(id)
+);
