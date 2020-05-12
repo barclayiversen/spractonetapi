5ad3e7971c716db0c719bb667936af04d0c855ab
Barclay SADA <barclay.iversen@sada.com>
2020-05-12 16:02:37 -0700

    SQL for get user posts and delete postd

--- repository/postRepository/post_psql.go
+++ repository/postRepository/post_psql.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"spractonetapi/models"
+	"strconv"
 	"time"
 )
 
@@ -19,4 +20,56 @@ func (p PostRepository) CreatePost(db *sql.DB, post models.Post) error {
 		return err
 	}
 	return nil
+}
+
+// GetUserPosts gets a user's posts by userid
+func (p PostRepository) GetUserPosts(db *sql.DB, userID int) ([]models.Post, error) {
+	var post models.Post
+	P := []models.Post{}
+	fmt.Println(userID)
+	stmt := "SELECT * FROM posts WHERE user_id = $1"
+	rows, err := db.Query(stmt, userID)
+	if err != nil {
+		fmt.Println(err)
+		return P, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		err = rows.Scan(&post.Id, &post.Title, &post.Post, &post.User_id, &post.Created_at)
+		if err != nil {
+			fmt.Println(err)
+			return P, err
+		}
+		P = append(P, post)
+	}
+
+	fmt.Println(P)
+	return P, nil
+}
+
+func (p PostRepository) DeletePost(db *sql.DB, userId int, postId string) error {
+	stmt := "SELECT user_id FROM posts WHERE id = $1"
+	id, err := strconv.Atoi(postId)
+	if err != nil {
+		return &MyError{}
+	}
+	row := db.QueryRow(stmt, id)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return err
+	// }
+	var post models.Post
+	row.Scan(&post.User_id)
+
+	if userId != post.User_id {
+		return &MyError{}
+	}
+	fmt.Println(row)
+	return nil
+}
+
+type MyError struct{}
+
+func (myErr *MyError) Error() string {
+	return "Something unexpected happened"
 }
